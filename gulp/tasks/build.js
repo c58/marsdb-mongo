049d4153deb9af4920282a5cb9bde02010595609
Artem Artemyev <aartemev@naumen.ru>
2016-01-29 16:15:18 +0500

    removed useless deps

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,13 +1,10 @@
 'use strict';
 
 var gulp = require('gulp');
-var babel = require('gulp-babel');
 var config = require('../config');
 var gulpif = require('gulp-if');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
-var browserify = require('browserify');
-var babelify = require('babelify');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var path = require('path');
